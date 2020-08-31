416973c2283271ffedbe991fe5d89fa4918c0e2e
dbernstein <dabylon@gmail.com>
2020-08-31 13:54:14 -0700

    Fixes mispelling of my last name.

--- src/main/java/org/fcrepo/upgrade/utils/RdfConstants.java
+++ src/main/java/org/fcrepo/upgrade/utils/RdfConstants.java
@@ -22,7 +22,7 @@ import java.util.Map;
 import java.util.Set;
 
 /**
- * @author dberstein
+ * @author dbernstein
  */
 public class RdfConstants {
     public static final String RDF_TYPE = "http://www.w3.org/1999/02/22-rdf-syntax-ns#type";
